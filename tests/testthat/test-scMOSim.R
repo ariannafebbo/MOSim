cc13889ddd92a70e21efd46251375e18d6207db0
Arianna Febbo <arianna.febbo@yahoo.it>
2023-05-10 10:06:10 +0200

    added tests for sc_omicSim function

--- tests/testthat/test-scMOSim.R
+++ tests/testthat/test-scMOSim.R
@@ -62,12 +62,53 @@ test_that("sc_omicSim returns a list", {
   expect_type(integration, "list")
 })
 
-test_that("sc_omicSim returns a list", {
-  expected <- c("activator","NE","NE","NE","activator","activator","activator","NE","NE","NE")
+test_that("sc_omicSim returns the expected subarray for the activity column", {
+  expected <- c("activator","repressor","NE","NE","NE","activator","repressor","activator","repressor","activator")
   omic_list <- sc_omicData(c("scRNA-seq","scATAC-seq"))
   cell_types <- list(cellA = c(1:20), cellB = c(161:191))
   sim <-sc_MOSim(omic_list, cell_types, numberCells = c(10,20), mean = c(2*10^6, 2*10^3), sd = c(10^3, 10^2))
   cell_types <- list(cellA= c(1:10), cellB = c(11:30))
   integration <- sc_omicSim(sim, cell_types, totalFeatures = 500)
   expect_equal(integration[["markers_cellA_cellB"]]$activity[1:10], expected)
+})
+
+
+test_that("sc_omicSim also works for a simulation with 4 cell types",{
+  rna_counts <- readRDS("../data/RNA_4CellTypes.rds")
+  atac_counts <- readRDS("../data/ATAC_4CellTypes.rds")
+  omicData_4cell <- sc_omicData(omics_types =  list("scRNA-seq","scATAC-seq"), data = list(rna_counts,atac_counts))
+  cell_types <- list(cellA = c(1:10), cellB = c(161:171), cellC =c(271:281), cellD=c(290:300))
+  sim_4cell <- sc_MOSim(omicData_4cell, cell_types, numberCells = c(10,20,10,20), mean = c(2*10^6, 2*10^3, 2*10^5, 2*10^6), sd = c(10^3, 10^2, 10^2, 10^3))
+  cell_types_integration <- list(cellA = c(1:10), cellB = c(11:30), cellC =c(31:40), cellD=c(41:60))
+  expect_type(sc_omicSim(sim_4cell, cell_types_integration, totalFeatures = 500), "list")
+})
+
+
+test_that("checking if sc_omicSim does not return two identical list for markers_cellA_cellB and markers_cellB_cellC",{
+  rna_counts <- readRDS("../data/RNA_4CellTypes.rds")
+  atac_counts <- readRDS("../data/ATAC_4CellTypes.rds")
+  omicData_4cell <- sc_omicData(omics_types =  list("scRNA-seq","scATAC-seq"), data = list(rna_counts,atac_counts))
+  cell_types <- list(cellA = c(1:10), cellB = c(161:171), cellC =c(271:281), cellD=c(290:300))
+  sim_4cell <- sc_MOSim(omicData_4cell, cell_types, numberCells = c(10,20,10,20), mean = c(2*10^6, 2*10^3, 2*10^5, 2*10^6), sd = c(10^3, 10^2, 10^2, 10^3))
+  cell_types_integration <- list(cellA = c(1:10), cellB = c(11:30), cellC =c(31:40), cellD=c(41:60))
+  omicSim <- sc_omicSim(sim_4cell, cell_types_integration, totalFeatures = 500)
+  expect_true(!identical(integration[[1]][[1]], integration[[2]][[1]]))
+})
+
+
+test_that("checking that passing the argument regulatorEffect to sc_omicSim subsets the activity column in the correct way",{
+  rna_counts <- readRDS("../data/RNA_4CellTypes.rds")
+  atac_counts <- readRDS("../data/ATAC_4CellTypes.rds")
+  omicData_4cell <- sc_omicData(omics_types =  list("scRNA-seq","scATAC-seq"), data = list(rna_counts,atac_counts))
+  cell_types <- list(cellA = c(1:10), cellB = c(161:171), cellC =c(271:281), cellD=c(290:300))
+  sim_4cell <- sc_MOSim(omicData_4cell, cell_types, numberCells = c(10,20,10,20), mean = c(2*10^6, 2*10^3, 2*10^5, 2*10^6), sd = c(10^3, 10^2, 10^2, 10^3))
+  
+  cell_types_integration <- list(cellA = c(1:10), cellB = c(11:30), cellC =c(31:40), cellD=c(41:60))
+  regulatorEffect <- list('activator' = 0.8,'repressor' = 0.1,'NE' = 0.1)
+  omicSim <- sc_omicSim(sim_4cell, cell_types_integration, totalFeatures = 500)
+  omicSim_subset <- sc_omicSim(sim_4cell, cell_types_integration, totalFeatures = 500, regulatorEffect = regulatorEffect)
+  NE <- sum(omicSim[["markers_cellA_cellB"]][["activity"]]=="NE")
+  NE_rounded <- round(NE*0.1)
+  NE_subset <- sum(omicSim_subset[["markers_cellA_cellB_subset"]][["activity"]] =="NE")
+  expect_true(NE_rounded == NE_subset)
 })
